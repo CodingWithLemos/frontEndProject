bcec3bbdcb432348358b426636b4da439c31fc83
Andre Lemos <andrelemoscodes@gmail.com>
2025-12-05 15:07:02 +0000

    4. QuizApp: add js file

---
+++ 4. QuizApp/index.js
@@ -0,0 +1,7 @@
+// create the feedback containers
+const answer_correct = document.getElementById("feedback_correct")
+const answer_incorrect = document.getElementById("feedback_incorrect")
+
+// create the button to proceed to the next question
+const btn_next = document.getElementById("next_btn")
+
